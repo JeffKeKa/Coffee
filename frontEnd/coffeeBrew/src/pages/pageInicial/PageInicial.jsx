43fe269d69f67a01972b02c8e22d75b6ed2b1ec6
Jefferson <jeffersonantunesdacunha@gmail.com>
2024-06-17 17:31:58 -0300

    jeff1706

--- frontEnd/coffeeBrew/src/pages/pageInicial/PageInicial.jsx
+++ frontEnd/coffeeBrew/src/pages/pageInicial/PageInicial.jsx
@@ -1,28 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
+import "./pageInicial.css";
+import { useState } from "react";
 
 function PageInicial() {
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        setIndex(prevIndex => (prevIndex + 1) % 3);
+      }, 3000);
+  
+      return () => {
+        clearInterval(intervalId);
+      };
+    }, []);
+
   return (
-    <div>
-      <div className="">
-        <div className="">
-          <img
-            // src="..\imagensProduto\imagensLogo\logoSemFundo.png"
-            className=""
-          />
-          <Link to="/Menu" className="botoesInicio">
-            Menu
-          </Link>
-          <Link to="/comanda" className="botoesInicio">
-            Pagamento
-          </Link>
-          <Link to="/comanda" className="botoesInicio">
-            Comanda
-          </Link>
+    <div className="conteinerPageInicial">
+      <div className="barraLateralPageInicial">
+        <img
+          // src="..\imagensProduto\imagensLogo\logoSemFundo.png"
+          className=""
+        />
+        <Link to="/Menu" className="botoesPageInicial">
+          Menu
+        </Link>
+        <Link to="/comanda" className="botoesPageInicial">
+          Pagamento
+        </Link>
+        <Link to="/comanda" className="botoesPageInicial">
+          Comanda
+        </Link>
+      </div>
+      <div className="direitaPageInicial">
+        <div className="carousel-slide" style={{ display: index === 0 ? 'block' : 'none' }}>
+          <img src="https://3.bp.blogspot.com/-gEIxd4Q5pU4/UTFWax73A3I/AAAAAAABr94/3DIoxR_NfnI/s1600/fotos-e-im%C3%A1genes-de-granos-de-caf%C3%A9-y-taza-coffee-photos-9.jpg"   className='logo' /> 
         </div>
-        <div className="direitaInicio"></div>
-        <p>PageInicial</p>
+
+        <div className="carousel-slide" style={{ display: index === 1 ? 'block' : 'none' }}>
+        <img src="https://media-cdn.tripadvisor.com/media/photo-s/11/c3/1c/19/20170924-134626-largejpg.jpg"   className='logo' /> 
+        </div>
+
+        <div className="carousel-slide" style={{ display: index === 2 ? 'block' : 'none' }}>
+        <img src="https://th.bing.com/th/id/OIP.0HRY6dUFgUyc1t-xyA1siAHaE8?rs=1&pid=ImgDetMain"   className='logo' /> 
+        </div>
+
       </div>
     </div>
   );

6da3481fd6fd4cb79d610120f70b3dc40ccc2dad
Jefferson <jeffersonantunesdacunha@gmail.com>
2024-06-19 16:47:02 -0300

    JEFF 190616

--- frontEnd/coffeeBrew/src/componentes/cardProduto/CardProduto.jsx
+++ frontEnd/coffeeBrew/src/componentes/cardProduto/CardProduto.jsx
@@ -1,27 +1,26 @@
-import "./cardProduto.css";
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import axios from "axios";
+import { UsuarioContext } from "../../context/GlobalContext";
+import "./cardProduto.css";
 
 function CardProduto() {
   const [produtos, setProdutos] = useState([]);
+  const { urlProdutos } = useContext(UsuarioContext);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(
-          "http://localhost:8010/coffebrew/produto"
-        );
+        const response = await axios.get(urlProdutos);
         setProdutos(response.data);
       } catch (error) {
         console.error("Error:", error);
       }
     };
     fetchData();
-  }, []);
+  }, [urlProdutos]); // Adicionamos urlProdutos como dependência para que useEffect seja reexecutado quando a URL mudar
 
   return (
-    <div>
+    <div className="menuCardPageMenu">
       {produtos.map((produto) => (
         <div key={produto.id} className="cardProdutoMenu">
           <img
@@ -29,11 +28,10 @@ function CardProduto() {
             className="imagemCardProduto"
             alt={produto.nome}
           />
-
           <div className="informaçãoCardProduto">
             <h1 className="nomeCardProduto">{produto.nome}</h1>
             <h5 className="descricaoCardProduto">{produto.descricao}</h5>
-            <div className="">
+            <div>
               <h1 className="">{produto.preco}</h1>
               <button className="">Comprar</button>
             </div>
@@ -42,30 +40,6 @@ function CardProduto() {
       ))}
     </div>
   );
-
-  // return (
-  //   <div>
-  //     <section className="cardProdutoMenu">
-  //       <img
-  //         src="./imagensProduto\imagensCafe\produtoCafeAmericano.jpg"
-  //         className="imagemCardProduto"
-  //       />
-
-  //       <div className="informaçãoCardProduto">
-  //         <h1 className="nomeCardProduto">CAFÉ AMERICANO</h1>
-  //         <h2 className="descricaoCardProduto">
-  //           O café americano é uma bebida feita diluindo-se espresso em água
-  //           quente, resultando em uma xícara suave e menos concentrada do que o
-  //           espresso puro.
-  //         </h2>
-  //         <section className="">
-  //           <h3 className="">R$30,00</h3>
-  //           <button className="">Comprar</button>
-  //         </section>
-  //       </div>
-  //     </section>
-  //   </div>
-  // );
 }
 
 export default CardProduto;
---
+++ frontEnd/coffeeBrew/src/service/apiProduto.jsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: 'http://localhost:8010/produto'
+})
+
+export const produtoCafe = async () => {
+    return api.post('/')
+}

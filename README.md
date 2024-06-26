b0245ee0f7a404de336a29ce088df0319a216996
Jefferson <jeffersonantunesdacunha@gmail.com>
2024-06-26 17:36:58 -0300

    atualizar

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 SA - 2024 SENAI
 
 React instalar npm 
+back 
+verificar caminhos do banco de dados
+no application do java
+spring.datasource.username=root
+spring.datasource.password=1991
---
+++ backEnd/SqlDBcoffebrew.sql
@@ -0,0 +1,9 @@
+-- database coffebrew;
+drop database coffebrew;
+create database coffebrew;
+use coffebrew;
+show databases;
+select * from clientes;
+select * from produtos;
+select * from pedidos;
+select * from comandas;
